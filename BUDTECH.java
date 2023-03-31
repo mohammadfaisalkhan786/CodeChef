1be8ff91e8773a8a3dd8a846009d1a4dd0187d69
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-03-31 09:52:12 +0530

    Create BUDTECH.java

---
+++ BUDTECH.java
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/BUDTECH
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int n=kk.nextInt()*1000;
+		    n=n/2;
+		    System.out.println(n/5);
+		    
+		}
+	}
+}
