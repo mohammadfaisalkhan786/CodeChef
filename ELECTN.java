ff5f33f6ed74b0b59ed04b4a3c40183d6479d3ae
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-04-08 09:30:28 +0530

    Create ELECTN.java

---
+++ ELECTN.java
@@ -0,0 +1,28 @@
+//https://www.codechef.com/problems/ELECTN
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int ans=0;
+		    int n=kk.nextInt();
+		    int x=kk.nextInt();
+		    for(int i=0;i<n;i++)
+		    {
+		        if(kk.nextInt()>=x)
+		        ans++;
+		    }
+		    System.out.println(ans);
+		}
+	}
+}
