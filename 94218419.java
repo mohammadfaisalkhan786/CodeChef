0eee443f38f8440244d0f1a7658c4c91cb2b801b
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-04-12 12:50:54 +0530

    Create 94218419.java

---
+++ 94218419.java
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/KNGTOR
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int n=kk.nextInt()*5;
+		    int m=kk.nextInt()*7;
+		    System.out.println(n+m);
+		}
+	}
+}
