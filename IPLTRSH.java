a42c01e212032b7dda17719b27b29ec26a481a91
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-04-13 09:02:56 +0530

    Create IPLTRSH.java

---
+++ IPLTRSH.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int n=kk.nextInt()-kk.nextInt();
+		    System.out.println((n<0)?"0":n);
+		}
+	}
+}
