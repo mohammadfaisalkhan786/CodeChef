dd4ca557d040068da3f5d5678b1b6c2d579b8ce1
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-04-14 09:15:34 +0530

    Create CANCHEF.java

---
+++ CANCHEF.java
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/CANCHEF
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int x=kk.nextInt()*15;
+		    int y=kk.nextInt()*2;
+		    System.out.println((x>=y)?"YES":"NO");
+		}
+	}
+}
