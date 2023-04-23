ac02247cbe7d3b9c7c04d1d74f8072d19cda59c5
mohammadfaisalkhan786 <117020639+mohammadfaisalkhan786@users.noreply.github.com>
2023-04-23 15:31:58 +0530

    Create BLACKCEL.java

---
+++ BLACKCEL.java
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/BLACKCEL
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+// 		int testcases=kk.nextInt();
+// 		while(testcases-->0)
+		{
+		    int n=kk.nextInt();
+		    System.out.println((n*n)/2);
+		}
+	}
+}
